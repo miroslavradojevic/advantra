2cc4a09c8ee22f5289c4b69aef8bafe41aab4666
miroslavradojevic <miroslav.radojevic@gmail.com>
2012-11-01 17:40:07 +0100

    updated reader for mean-shift outputs

--- ms-convergence-read/read_ms_conv.m
+++ ms-convergence-read/read_ms_conv.m
@@ -5,6 +5,7 @@ input_file_dir  = input('folder path? (where ms_convergence_points.m is):  ','s'
 cd(input_file_dir);
 disp('loading...');
 ms_log;
+disp('loading finished.');
 fullscreen = get(0,'ScreenSize');
 screen_w = fullscreen(3);
 screen_h = fullscreen(4);
@@ -18,36 +19,56 @@ if(isdir('frames')),
     rmdir('frames', 's');
 end
 
-mkdir frames;
+if(isdir('msvecs')),
+    rmdir('msvecs', 's');
+end
 
+if(isdir('hists')),
+    rmdir('hists', 's');
+end
+
+mkdir frames;
+mkdir msvecs;
+mkdir hists;
+
+im = imread('ms_input_image.tif');
 frames = length(new_pos);
-for fr = 1:frames,
-    figure(fr);
-    imshow(imread('ms_input_image.tif')); hold on;
-    if fr==1,
-        plot(start_pos(:,2),start_pos(:,1),'b.');
-    else
-        plot(new_pos{1,fr}(:,2), new_pos{1,fr}(:,1),'b.'); 
-        
-        %frame_diff = (new_pos{1,fr} - new_pos{1,fr-1});
-        %ep = max(sqrt(sum(frame_diff.^2,2)));
-        
-        export_fig(strcat('./frames/iter', num2str(fr), '.tif'), '-tif', '-a1', '-native');
-    end
+curr_clus_nr = zeros(frames);
+
+[X_plot,Y_plot] = meshgrid(1:size(d{1,1},2), 1:size(d{1,1},1));
+
+for fr = 1 : frames,
     
+    figure(fr);
+    imshow(im); hold on;
+    plot(new_pos{1,fr}(:,2), new_pos{1,fr}(:,1),'b.'); 
+    export_fig(strcat('./frames/iter', num2str(fr), '.tif'), '-tif', '-a1', '-native');
     close(fr);
-end    
+    
+    d{1,fr} = sqrt(d{1,fr});
+    
+    figure(fr);
+    mesh(X_plot,Y_plot, d{1,fr} );
+    export_fig(strcat('./msvecs/mean_shift_vecs', num2str(fr), '.png'), '-png', '-a1', '-native');%, '-transparent'
+    close(fr);
+    
+    figure(fr);
+    [N(fr,:), X(fr,:)] = hist(d{1,fr}(:), 100); 
+    bar(X(fr,:),N(fr,:));
+    export_fig(strcat('./hists/his', num2str(fr), '.png'), '-png', '-a1', '-native');%, '-transparent'
+    close(fr);
+    
+    curr_clus_nr(fr) = sum(N(fr,:)>0);
+    
+end  
+
+figure;
+plot(curr_clus_nr); 
+xlabel('iteration');
+ylabel('bins'' number');
+grid on;
+
+export_fig('stopping_point.png', '-png', '-a1', '-native', '-transparent');
 
 cd(current_dir);
 disp('done.');
-% % visualize
-% nr_frames = size(frames, 2);
-% for iter = 1 : nr_frames,
-%     figure(iter);
-%     % [row col] is stored
-%     imshow(im);
-%     hold on;
-%     plot(frames{1,iter}(1:2:end,2), frames{1,iter}(1:2:end,1), 'r.');
-%     export_fig(strcat('frame', num2str(iter),'.tif'), '-tif', '-a1');
-%     close(iter);
-% end
