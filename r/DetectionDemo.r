05ba4741bf381889599b5f31af1f565ba9fc8652
Miroslav Radojević <miroslav.radojevic@gmail.com>
2013-09-24 18:53:45 +0200

    changed scripts and r plot to export values to latex table (isbi submission)

--- r/DetectionDemo.r
+++ r/DetectionDemo.r
@@ -6,6 +6,9 @@ rm(list = ls())
 # set working directory to current source directory
 this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
+
+print(this.dir)
+
 #X11()
 ######## data #########
 fileNameData = "data.dat"
@@ -16,7 +19,7 @@ legendData=read.table(fileNameLegend)
 
 legendData2 <- c() #(data="",nrow=1,ncol=2*length(legendData))
 for (name in names(legendData)) {
-    legendData2 <- c(legendData2, as.character(as.character(legendData[[name]])), "")
+  legendData2 <- c(legendData2, as.character(as.character(legendData[[name]])), "")
 }
 
 nrElements  = dim(tableData)[2]
@@ -29,7 +32,7 @@ loopAll <- 1:nrElements
 every1 <- loopAll[seq(1, length(loopAll), 3)]
 every2 <- loopAll[1+seq(1, length(loopAll), 3)]
 every3 <- loopAll[2+seq(1, length(loopAll), 3)]
-L = length(every1)
+L = length(every1) # nr of folders
 
 # add last row,sum per each column
 tableDataCum = colSums (tableData, na.rm = FALSE, dims = 1)
@@ -37,15 +40,17 @@ tableDataCum = colSums (tableData, na.rm = FALSE, dims = 1)
 tableData = rbind(tableData, tableDataCum)
 
 # fill the values for output with zeros
-plotData <- matrix(data=0,nrow=3,ncol=2*L)
+plotData <- matrix(data=0, nrow=3,ncol=2*L)  # plotData for barplot
 
 for (imageIdx in 1:(nrImages+1)) {
-  #print(imageIdx)
+
+  print(imageIdx)
 
   for (i in 1:L) {
-    a = tableData[imageIdx, every1[i]]
-    b = tableData[imageIdx, every2[i]]
-    c = tableData[imageIdx, every3[i]]
+
+    a = tableData[imageIdx, every1[i]] # TP
+    b = tableData[imageIdx, every2[i]] # FP
+    c = tableData[imageIdx, every3[i]] # FN
 
     #print(c(a, b, c))
 
@@ -55,8 +60,8 @@ for (imageIdx in 1:(nrImages+1)) {
     plotData[3,2*i  ] = b/(a+c)
 
     #plotData[1,2*i-1] = a
-    #plotData[2,2*i-1] = b
-    #plotData[3,2*i  ] = c
+    #plotData[2,2*i-1] = c
+    #plotData[3,2*i  ] = b
 
   }
 
@@ -65,6 +70,10 @@ for (imageIdx in 1:(nrImages+1)) {
   }
   else {
     filename = paste("alltogether.pdf", sep="")
+
+    # in case of the last row (sum) add the values to plotDataTable so that each column corresponds to one folder (category)
+
+
   }
 
   pdf(file=filename, width = 5, height = 2) # in inches
@@ -75,8 +84,8 @@ for (imageIdx in 1:(nrImages+1)) {
   colors=c(rgb(0,0,1,0.3),rgb(1,0,0,0.3),rgb(0,1,0,0.3))
   bardensity <- t(t(c(15,15,0)))
   barangle <- t(t(c(45,135,0)))
-  spacing <-c(.75, .15) 
-  
+  spacing <-c(.75, .15)
+
   barplot(plotData,
           space=spacing,
           #xlab = "CONFIGURATION",
@@ -85,7 +94,7 @@ for (imageIdx in 1:(nrImages+1)) {
           #legend.text = c("TP", "FP","FN"),
           col = colors,
           beside=FALSE
-          
+
   )
   barplot(plotData,
           space=spacing,
@@ -95,13 +104,38 @@ for (imageIdx in 1:(nrImages+1)) {
           #legend.text = c("TP", "FP","FN"),
           col = "black",
           beside=FALSE,
-	  density = bardensity,
-	  angle = barangle,
+          density = bardensity,
+          angle = barangle,
           add=T
   )
-  
+
   # Add legend to top right, outside plot region
   legend("topright", inset=c(-0.08,0), c("TP", "FN","FP"), fill= colors, bty = "n")
   dev.off()
 
+
+
 }
+# print last row for report table every 3rd goes to one column
+print("table data")
+print(tableData)
+plotDataTable <- matrix(data=0, nrow=3,ncol=2*L) # plot in the table
+
+for (i in 1:L) {
+
+  a = tableData[nrImages+1, every1[i]] # TP
+  b = tableData[nrImages+1, every2[i]] # FP
+  c = tableData[nrImages+1, every3[i]] # FN
+
+  plotDataTable[1,2*i-1] = a;
+  plotDataTable[2,2*i-1] = b;
+  plotDataTable[3,2*i-1] = c;
+
+      plotDataTable[1,2*i] = a/(a+c);
+      plotDataTable[2,2*i] = b/(a+c);
+      plotDataTable[3,2*i] = c/(a+c);
+
+}
+
+print("table data")
+print(plotDataTable)
