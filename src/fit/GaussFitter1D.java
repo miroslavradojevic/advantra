aec4c344c057553c5a02090cbd7982845c7d9635
Miroslav Radojević <miroslav.radojevic@gmail.com>
2014-02-07 18:46:44 +0100

    ...

---
+++ src/fit/GaussFitter1D.java
@@ -0,0 +1,27 @@
+package fit;
+
+import ij.process.ImageProcessor;
+
+/**
+ * Created by miroslav on 2/7/14.
+ * Fitting gaussian 1d profile on 2d image patch
+ * fit gaussian function on average or weighted average per column
+ *
+ */
+public class GaussFitter1D {
+
+    public GaussFitter1D(){
+
+        //
+
+
+    }
+
+    public float ssd(ImageProcessor patch, boolean weighted) {
+
+        //
+
+
+    }
+
+}
