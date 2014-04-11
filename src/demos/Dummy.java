757f86c8e3ca43b8512afd8573a724c21e50f1fe
Miroslav Radojević <miroslav.radojevic@gmail.com>
2014-04-11 10:56:30 +0200

    ...

--- src/demos/Dummy.java
+++ src/demos/Dummy.java
@@ -2,6 +2,7 @@ package demos;
 
 import aux.Stat;
 import detection2d.FuzzyBranch2D;
+import ij.plugin.PlugIn;
 
 import java.util.Arrays;
 
@@ -9,20 +10,55 @@ import java.util.Arrays;
  * Created by miroslav on 3/25/14.
  * java -cp "$HOME/critpoint/*:$HOME/jarlib/*" demos.Dummy
  */
-public class Dummy {
+public class Dummy implements PlugIn {
+
+
+
 	public static void main(String[] args) {
+
+		demo();
+
+	}
+
+	public void run(String s) {
+
+		demo();
+
+	}
+
+	public static void demo()
+	{
 		System.out.println("Dummy working...");
 		float[] ar = new float[]{1, 2, 3, 4, 5};
 		System.out.println(Arrays.toString(ar));
 		Stat.normalize(ar);
 		System.out.println(Arrays.toString(ar));
 
-        FuzzyBranch2D test = new FuzzyBranch2D(20, 1, 0.1f, 0.6f, 0.1f);
-        test.showFuzzification();
-        test.showDefuzzification();
-        float[] out = new float[test.agg.length];
-        test.critpointScores(1f, 1f, out);
-        test.showAgg();
+		FuzzyBranch2D test = new FuzzyBranch2D(			40,
+													  // ncc
+													  1, 0.2f,      // high
+													  0.4f, 0.2f,   // low
+													  // lhood
+													  0.85f, 0.25f, // high
+													  0.0f, 0.25f,  // low
+													  .25f  // std output membership functions - defines separation
+		);
+		test.showFuzzification();
+		test.showDefuzzification();
 
+
+		float[] temp = new float[2];
+		System.out.print("(1, 1) -> " + test.branchStrengthDefuzzified(1f, 1f));
+		test.branchStrengthFuzzified(1f, 1f, temp);
+		System.out.println(" -> " + Arrays.toString(temp));
+		test.showAgg();
+
+		System.out.print("(.9, .9) -> " + test.branchStrengthDefuzzified(.9f, .9f));
+		test.branchStrengthFuzzified(.9f, .9f, temp);
+		System.out.println(" -> " + Arrays.toString(temp));
+		test.showAgg();
+
+		test.showDefuzzificationSurface();
 	}
+
 }
--- src/detection2d/FuzzyBranch2D.java
+++ src/detection2d/FuzzyBranch2D.java
@@ -1,6 +1,9 @@
 package detection2d;
 
+import ij.ImagePlus;
+import ij.ImageStack;
 import ij.gui.Plot;
+import ij.process.FloatProcessor;
 
 import java.awt.*;
 import java.util.Arrays;
@@ -10,71 +13,81 @@ import java.util.Arrays;
  */
 public class FuzzyBranch2D {
 
-    // two inputs of different nature, with different parameters: ncc, likelihood
+    // two inputs ncc, likelihood, can be high or low
     float ncc_start = -1; // define the range
     float ncc_end = +1;
-    float    mu_ncc, sig_ncc;
+    float ncc_high_mean;
+	float ncc_high_sigma;
+	float ncc_low_mean;
+	float ncc_low_sigma;
 
-    float start_likelihood = 0;
-    float end_likelihood = 1;
-    float    sig_likelihood, mu_likelihood;
+    float likelihood_start = 0;
+    float likelihood_end = 1;
+	float likelihood_high_mean;
+	float likelihood_high_sigma;
+	float likelihood_low_mean;
+	float likelihood_low_sigma;
 
-    // one output: strength
-    float    mu_on=1f, mu_off=0f, sig_on=0.3f, sig_off=0.3f;
-    float    ;
+    // one output can be off or on
+	float   output_on_mean = 1f;
+	float 	output_on_sigma;
+	float 	output_off_mean = 0f;
+	float	output_off_sigma;
+	float 	output_start;// = output_off_mean-3*output_off_sigma;
+	float 	output_end;// = output_on_mean+3*output_on_sigma;
 
     int      N;         // number of points to aggregate
-//    int      L = 1;		// number of outputs - saying how much it is on or off
-    int[]    out_idxs;  // indexes of out points
 
     // fuzzification parameters
-         // high
-    // todo add low separate parameters so that low has it's own function
-
-
-    float[] 	x;      	    // serve as x axis for membership functions (range 0 to 4)
+    float[] 	x;      	    // serve as x axis for membership, aggregation functions (range output_start to output_end)
     public float[] 	agg;            // serve as aggregation
 
-    // L outputs
+    // output categories
     float[] 	v_off;
     float[] 	v_on;
 
-    // membership fuzzy sets for L outputs (rules will call these categories "OFF", "ON")
+    // membership fuzzy sets for output (rules will call these categories "OFF", "ON")
     float[] q_off;
     float[] q_on;
 
-    public FuzzyBranch2D(int _N, float _mu_ncc, float _sig_ncc, float _mu_likelihood, float _sig_likelihood)
+    public FuzzyBranch2D(
+								int _N,
+								float _ncc_high_mean,
+								float _ncc_high_sigma,
+								float _ncc_low_mean,
+								float _ncc_low_sigma,
+								float _likelihood_high_mean,
+								float _likelihood_high_sigma,
+								float _likelihood_low_mean,
+								float _likelihood_low_sigma,
+								float _output_on_sigma
+								)
     {
         N = _N;
+        ncc_high_mean = _ncc_high_mean;
+        ncc_high_sigma = _ncc_high_sigma;
+        ncc_low_mean = _ncc_low_mean;
+        ncc_low_sigma = _ncc_low_sigma;
+		likelihood_high_mean = _likelihood_high_mean;
+		likelihood_high_sigma = _likelihood_high_sigma;
+		likelihood_low_mean = _likelihood_low_mean;
+		likelihood_low_sigma = _likelihood_low_sigma;
+		output_on_sigma = _output_on_sigma;
+		output_off_sigma = output_on_sigma;
+		output_start = output_off_mean-3*output_off_sigma;
+		output_end = output_on_mean+3*output_on_sigma;
 
-        mu_likelihood = _mu_likelihood;
-        sig_likelihood = _sig_likelihood;
-
-        mu_ncc = _mu_ncc;
-        sig_ncc = _sig_ncc;
-
-        x = new float[N]; // output range mu_off-3*sig_off, mu_on+3*sig_on
-        for (int i=0; i<N; i++) x[i] = (mu_off-3*sig_off) + i * ((float)(L-1) / (N-1));
-
-        System.out.println(Arrays.toString(x));
-
-        out_idxs = new int[L];
-        int step = (N-1)/(L-1);
-        for (int i=0; i<L; i++) {
-            out_idxs[i] = i*step;
-            System.out.println("idx " + out_idxs[i]);
-        }
+        x = new float[N]; // output range mu_off-3*sig_off, mu_on+3*sig_on , also used in centroid calculation
+        for (int i=0; i<N; i++) x[i] = output_start + i * ( (output_end-output_start) / (N-1));
 
         // output membership functions
         q_off = new float[N];
-        for (int i=0; i<N; i++) {
-            q_off[i] = (float) Math.exp(-Math.pow(x[i] - mu_off, 2)/(2*Math.pow(sig_off, 2)));
-        }
+        for (int i=0; i<N; i++)
+			q_off[i] = (float) Math.exp(-Math.pow(x[i] - output_off_mean, 2) / (2 * Math.pow(output_off_sigma, 2)));
 
         q_on = new float[N];
-        for (int i=0; i<N; i++) {
-            q_on[i] = (float) Math.exp(-Math.pow(x[i] - mu_on, 2)/(2*Math.pow(sig_on, 2)));
-        }
+        for (int i=0; i<N; i++)
+			q_on[i] = (float) Math.exp(-Math.pow(x[i] - output_on_mean, 2) / (2 * Math.pow(output_on_sigma, 2)));
 
         // aux arrays used for calculations
         agg = new float[N];
@@ -88,26 +101,38 @@ public class FuzzyBranch2D {
      */
     private float h_ncc_high(float ncc)
     {
-        if (ncc>=mu_ncc)
+        if (ncc>=ncc_high_mean)
             return 1;
-        else //if(theta>mu_ON) //  && theta<=mu_OFF
-            return (float) Math.exp(-Math.pow(ncc - mu_ncc, 2)/(2*Math.pow(sig_ncc, 2)));
-        //else
-        //    return 0;
+        else
+            return (float) Math.exp(-Math.pow(ncc - ncc_high_mean, 2)/(2*Math.pow(ncc_high_sigma, 2)));
     }
+
+	private float h_ncc_low(float ncc)
+	{
+		if (ncc<=ncc_low_mean)
+			return 1;
+		else
+			return (float) Math.exp(-Math.pow(ncc - ncc_low_mean, 2)/(2*Math.pow(ncc_low_sigma, 2)));
+	}
 
     /*
     fuzzification of the likelihood input
      */
     private float h_likelihood_high(float likelihood)
     {
-        if (likelihood>=mu_likelihood)
+        if (likelihood>=likelihood_high_mean)
             return 1;
-        else //if(theta>mu_ON) //  && theta<=mu_OFF
-            return (float) Math.exp(-Math.pow(likelihood - mu_likelihood, 2)/(2*Math.pow(sig_likelihood, 2)));
-        //else
-        //    return 0;
+        else
+            return (float) Math.exp(-Math.pow(likelihood - likelihood_high_mean, 2)/(2*Math.pow(likelihood_high_sigma, 2)));
     }
+
+	private float h_likelihood_low(float likelihood)
+	{
+		if (likelihood<=likelihood_low_mean)
+			return 1;
+		else
+			return (float) Math.exp(-Math.pow(likelihood - likelihood_low_mean, 2)/(2*Math.pow(likelihood_low_sigma, 2)));
+	}
 
     public void showFuzzification()
     {
@@ -118,12 +143,12 @@ public class FuzzyBranch2D {
         float[] yy_on = new float[NN];
         float[] yy_off = new float[NN];
 
-        for (int ii=0; ii<xx.length; ii++) xx[ii] = -1 + ii * ( (+1f-(-1)) / (NN-1));
+        for (int ii=0; ii<xx.length; ii++) xx[ii] = ncc_start + ii * ( (ncc_end-ncc_start) / (NN-1));
 
         // on
         for (int ii=0; ii<xx.length; ii++) yy_on[ii] = h_ncc_high(xx[ii]);
         //off
-        for (int ii=0; ii<xx.length; ii++) yy_off[ii] = 1-h_ncc_high(xx[ii]);
+        for (int ii=0; ii<xx.length; ii++) yy_off[ii] = h_ncc_low(xx[ii]);
 
         Plot p = new Plot("ncc fuzzification", "", "", xx, yy_on, Plot.LINE);
         p.setColor(Color.RED);
@@ -138,7 +163,7 @@ public class FuzzyBranch2D {
         // on
         for (int ii=0; ii<xx.length; ii++) yy_on[ii] = h_likelihood_high(xx[ii]);
         //off
-        for (int ii=0; ii<xx.length; ii++) yy_off[ii] = 1-h_likelihood_high(xx[ii]);
+        for (int ii=0; ii<xx.length; ii++) yy_off[ii] = h_likelihood_low(xx[ii]);
 
         p = new Plot("likelihood fuzzification", "", "", xx, yy_on, Plot.LINE);
         p.setColor(Color.RED);
@@ -165,6 +190,40 @@ public class FuzzyBranch2D {
         p.show();
     }
 
+	public void showDefuzzificationSurface(){
+
+		int w = 200; // -1 / 1
+		int h = 100; //  0 / 1
+
+		ImageStack is_out = new ImageStack(w, h);
+
+		float[][] t = new float[w][h];
+		float[][] t_off = new float[w][h];
+		float[][] t_on = new float[w][h];
+		float[] dummy = new float[2];
+
+		for (int x=0; x<w; x++) {
+
+			for (int y=0; y<h; y++) {
+
+				float ncc = ncc_start + x * ((ncc_end-ncc_start)/(w-1));
+				float likelihood = likelihood_start + y * ((likelihood_end-likelihood_start)/(h-1));
+				t[x][y] = branchStrengthDefuzzified(ncc, likelihood);
+				branchStrengthFuzzified(t[x][y], dummy);
+				t_off[x][y] = dummy[0];
+				t_on[x][y] = dummy[1];
+
+			}
+
+		}
+
+		is_out.addSlice("branch strength defuzzified", new FloatProcessor(t));
+		is_out.addSlice("branch strength off", new FloatProcessor(t_off));
+		is_out.addSlice("branch strength on", new FloatProcessor(t_on));
+
+		new ImagePlus("output", is_out).show();
+
+	}
 
     private float[] fi_off(float z)
     {
@@ -192,24 +251,83 @@ public class FuzzyBranch2D {
         return v_on;
     }
 
-    public void critpointScores(float ncc_in, float likelihood_in, float[] out)
+    public float branchStrengthDefuzzified(float ncc_in, float likelihood_in)
     {
         Arrays.fill(agg, 0);
         float[] cur;
         float mu;
 
         // apply rules
-        mu = min (1-h_ncc_high(ncc_in),   1-h_likelihood_high(likelihood_in)  ) ; cur = fi_off(mu); accumulate(cur, agg);
-        System.out.println("00:"+mu); System.out.println("ncc :"+h_ncc_high(ncc_in)+" likelihood "+h_likelihood_high(likelihood_in));
-        mu = min (1-h_ncc_high(ncc_in),   h_likelihood_high(likelihood_in)    ) ; cur = fi_off(mu); accumulate(cur, agg); System.out.println("01:" + mu);
+//        mu = min (h_ncc_low(ncc_in),   		h_likelihood_low(likelihood_in)  ) ; cur = fi_off(mu); accumulate(cur, agg);
+//        mu = min (h_ncc_low(ncc_in),   		h_likelihood_high(likelihood_in) ) ; cur = fi_off(mu); accumulate(cur, agg);
+//        mu = min (h_ncc_high(ncc_in),     	h_likelihood_low(likelihood_in)  ) ; cur = fi_off(mu); accumulate(cur, agg);
 
-        mu = min (h_ncc_high(ncc_in),     1-h_likelihood_high(likelihood_in)  ) ; cur = fi_off(mu); accumulate(cur, agg); System.out.println("10:" + mu);
-        mu = min (h_ncc_high(ncc_in),     h_likelihood_high(likelihood_in)    ) ; cur = fi_on(mu);  accumulate(cur, agg); System.out.println("11:" + mu);
+		mu = h_ncc_low(ncc_in) ; cur = fi_off(mu); accumulate(cur, agg);
+		mu = h_likelihood_low(likelihood_in); cur = fi_off(mu); accumulate(cur, agg);
+		mu = min (h_ncc_high(ncc_in),     	h_likelihood_high(likelihood_in) ) ; cur = fi_on(mu);  accumulate(cur, agg);  // and
         // finished rules
 
-        for (int i=0; i<L; i++) out[i] = agg[out_idxs[i]];
+		float centroid = 0;
+		float norm = 0;
+		for (int i=0; i<N; i++) {
+			centroid += x[i]*agg[i];
+			norm += agg[i];
+		}
+
+		return centroid/norm;
 
     }
+
+	public void branchStrengthFuzzified(float ncc_in, float likelihood_in, float[] output_off_on) // [off_score, on_score]
+	{
+		float defuzz = branchStrengthDefuzzified(ncc_in, likelihood_in);
+		// use the same formulas that were used for q_on/q_off
+		output_off_on[0] = (float) Math.exp(-Math.pow(defuzz - output_off_mean, 2) / (2 * Math.pow(output_off_sigma, 2)));
+		output_off_on[1] = (float) Math.exp(-Math.pow(defuzz - output_on_mean, 2) / (2 * Math.pow(output_on_sigma, 2)));
+	}
+
+	public void branchStrengthFuzzified(float _defuzz, float[] output_off_on) // [off_score, on_score]
+	{
+//		float defuzz = branchStrengthDefuzzified(ncc_in, likelihood_in);
+		// use the same formulas that were used for q_on/q_off
+		output_off_on[0] = (float) Math.exp(-Math.pow(_defuzz - output_off_mean, 2) / (2 * Math.pow(output_off_sigma, 2)));
+		output_off_on[1] = (float) Math.exp(-Math.pow(_defuzz - output_on_mean, 2) / (2 * Math.pow(output_on_sigma, 2)));
+	}
+
+	public float endpointDefuzzified(float ncc_1, float likelihood_1)
+	{
+
+		float[] t = new float[2];
+
+		branchStrengthFuzzified(ncc_1, likelihood_1, t);
+		float b1_is_off = t[0];
+		float b1_is_on = t[1];
+
+		// rules if there is one
+		float mu;
+		float[] cur;
+
+		mu = b1_is_off; cur = fi_off(mu); accumulate(cur, agg);
+		mu = b1_is_on; cur = fi_on(mu); accumulate(cur, agg);
+
+		float centroid = 0;
+		float norm = 0;
+		for (int i=0; i<N; i++) {
+			centroid += x[i]*agg[i];
+			norm += agg[i];
+		}
+
+		return centroid/norm;
+
+	}
+
+	public float endpointDefuzzified(float ncc_1, float likelihood_1,
+									 float ncc_2, float likelihood_2)
+	{
+
+	}
+
+
 
     public static final float min(float in1, float in2)
     {
